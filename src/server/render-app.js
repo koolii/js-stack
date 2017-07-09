ca2e3cae3afad7a6bdac6358d76c5727c2d59aac
koolii <koolii0909@gmail.com>
2017-07-10 08:26:46 +0900

    add webpack installment

--- src/server/render-app.js
+++ src/server/render-app.js
@@ -1,19 +1,7 @@
 // @flow
 
-import { STATIC_PATH } from '../shared/config'
-
-//  const renderApp = (title: string) =>
-//  `<!doctype html>
-//  <html>
-//    <head>
-//      <title>${title}</title>
-//      <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
-//    </head>
-//    <body>
-//      <h1>${title}</h1>
-//    </body>
-//  </html>
-//  `
+import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
+import { isProd } from '../shared/util'
 
 const renderApp = (title: string) =>
   `
@@ -24,7 +12,8 @@ const renderApp = (title: string) =>
     <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
   </head>
   <body>
-    <h1>${title}</h1>
+    <div class="${APP_CONTAINER_CLASS}"></div>
+    <script src="${isProd ? STATIC_PATH : `http://localhost:${WDS_PORT}/dist`}/js/bundle.js"></script>
   </body>
 </html>
   `
