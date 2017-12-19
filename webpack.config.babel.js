1256cc30b81bc14ff0f7c7856907ddd3ad700ef5
koolii <koolii0909@gmail.com>
2017-12-19 21:49:41 +0100

    :+1: add webpack.config.babel.js explain

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,24 +5,24 @@ import { WDS_PORT } from './src/shared/config'
 import { isProd } from './src/shared/util'
 
 export default {
-  entry: [
+  entry: [ // appのスタートポイント
     './src/client',
   ],
   output: {
-    filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js', // バンドル後のファイル名
+    path: path.resolve(__dirname, 'dist'), // バンドルしたファイルを配置する先
     publicPath: isProd ? '/static/' : `http://localhost:${WDS_PORT}/dist/`,
   },
   module: {
-    rules: [
+    rules: [ // 対象ファイルをtestで設定し、useで使用するバイナリ？、excludeは除外するファイルのパス
       { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
     ],
   },
   devtool: isProd ? false : 'source-map',
-  resolve: {
+  resolve: { // 対象となるファイル(module.rules[0].testとどういう違いがあるのか)
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    port: WDS_PORT,
+    port: WDS_PORT, // 開発するwebpack-dev-serverポート番号
   },
 }
