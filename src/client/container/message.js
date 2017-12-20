4b4a355f1828d2d973dd80a4d9b96947116c0f13
koolii <kuriyama@createentrance.com>
2017-12-20 12:29:40 +0100

    :sparkles: add Redux explains about combineReducers and createStore

--- src/client/container/message.js
+++ src/client/container/message.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import Message from '../component/message'
 
 const mapStateToProps = state => ({
+  // ここのstate.helloと言うのはsrc/index.jsxのcombineReducers()で対応付けがされている
   message: state.hello.get('message'),
 })
 
