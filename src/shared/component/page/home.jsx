5eaec52e7862188c6371174cec5d9c9367672901
koolii <koolii0909@gmail.com>
2017-12-24 23:56:40 +0100

    :shower: remove unnecessary line

--- src/shared/component/page/home.jsx
+++ src/shared/component/page/home.jsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import injectSheet from 'react-jss'
-
 import ModalExample from '../modal-example'
 import { APP_NAME } from '../../config'
 
